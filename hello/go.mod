570f04b0332201994a3616769e91f9d4e5431a40
lenimbugua <lenimbugua@gmail.com>
2022-02-16 12:45:42 +0300

    learning how to include packages

---
+++ hello/go.mod
@@ -0,0 +1,3 @@
+module github.com/lenimbugua/go-tutorial/hello
+
+go 1.17
---
+++ hello/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/lenimbugua/go-tutorial/greetings"
+)
+
+func main() {
+	message := greetings.Hello("Gladys")
+	fmt.Println(message)
+
+}
